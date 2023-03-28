086be45e1b7672c4cf4d0c4c96cc507cbb2ad3fb
1nickochieng <1nickochieng@gmail.com>
2023-03-28 14:19:51 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from credentials import mobile_number
+import requests
+import schedule
+import time
+
+def send_message():
+    resp = requests.post('https://textbelt.com/text', {
+        'phone': mobile_number,
+        'message': 'Hey, My jaber',
+        'key': 'textbelt'
+    })
+    print(resp.json())
+    
+#schedule.every().day.at('14:00').do(send_message)
+
+schedule.every(10).seconds.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
